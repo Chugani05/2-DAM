56617c3b0352bc6049eaefd44622653cbce7b0b9
Chugani05 <rashi.chugani.n@gmail.com>
2025-11-06 01:34:41 +0000

    crudinterface done

--- AccesoADatos/unidad02/tareas/tarea01/hotel/src/main/java/org/docencia/hotel/persistence/jpa/interfaces/CrudInterface.java
+++ AccesoADatos/unidad02/tareas/tarea01/hotel/src/main/java/org/docencia/hotel/persistence/jpa/interfaces/CrudInterface.java
@@ -1,5 +1,11 @@
 package org.docencia.hotel.persistence.jpa.interfaces;
 
-public interface CrudInterface {
+import java.util.List;
 
+public interface CrudInterface<TYPE, ID> {
+    public boolean exists(ID id);
+    public TYPE findById(ID id);
+    public List<TYPE> findAll();
+    public TYPE save(TYPE entity);
+    public boolean delete(ID id);
 }
