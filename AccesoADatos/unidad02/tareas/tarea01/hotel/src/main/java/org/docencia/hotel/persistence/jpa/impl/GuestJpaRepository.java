7d69e5b4b331d450018de19d8341c3fc1be5fc4c
Chugani05 <rashi.chugani.n@gmail.com>
2025-11-07 19:57:30 +0000

    implement save and jpa interfaces

--- AccesoADatos/unidad02/tareas/tarea01/hotel/src/main/java/org/docencia/hotel/persistence/jpa/impl/GuestJpaRepository.java
+++ AccesoADatos/unidad02/tareas/tarea01/hotel/src/main/java/org/docencia/hotel/persistence/jpa/impl/GuestJpaRepository.java
@@ -1,15 +1,25 @@
 package org.docencia.hotel.persistence.jpa.impl;
 
+import java.util.UUID;
+
 import org.docencia.hotel.domain.repository.IGuestRepository;
 import org.docencia.hotel.model.Guest;
 import org.docencia.hotel.persistence.jpa.abstrats.JpaAbstractRepository;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.docencia.hotel.persistence.jpa.interfaces.IGuestJpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class GuestJpaRepository extends JpaAbstractRepository<Guest, String> implements IGuestRepository{
 
-    public GuestJpaRepository(JpaRepository<Guest, String> repository) {
+    public GuestJpaRepository(IGuestJpaRepository repository) {
         super(repository);
+    }
+
+    @Override
+    public Guest save(Guest guest) {
+        if (guest.getId() == null) {
+            guest.setId(UUID.randomUUID().toString());
+        }
+        return super.repository.save(guest);
     }
 }
--- AccesoADatos/unidad02/tareas/tarea01/hotel/src/main/java/org/docencia/hotel/persistence/jpa/impl/HotelJpaRepositor.java
+++ AccesoADatos/unidad02/tareas/tarea01/hotel/src/main/java/org/docencia/hotel/persistence/jpa/impl/HotelJpaRepositor.java
@@ -1,15 +1,25 @@
 package org.docencia.hotel.persistence.jpa.impl;
 
+import java.util.UUID;
+
 import org.docencia.hotel.domain.repository.IHotelRepository;
 import org.docencia.hotel.model.Hotel;
 import org.docencia.hotel.persistence.jpa.abstrats.JpaAbstractRepository;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.docencia.hotel.persistence.jpa.interfaces.IHotelJpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class HotelJpaRepositor extends JpaAbstractRepository<Hotel, String> implements IHotelRepository{
 
-    public HotelJpaRepositor(JpaRepository<Hotel, String> repository) {
+    public HotelJpaRepositor(IHotelJpaRepository repository) {
         super(repository);
+    }
+
+    @Override
+    public Hotel save(Hotel hotel) {
+        if (hotel.getId() == null) {
+            hotel.setId(UUID.randomUUID().toString());
+        }
+        return super.repository.save(hotel);
     }   
 }
