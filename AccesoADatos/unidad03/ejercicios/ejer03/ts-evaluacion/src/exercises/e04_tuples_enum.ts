94901b7c6122a47dfa3c0b22778540d1e5f21f9f
Chugani05 <rashi.chugani.n@gmail.com>
2026-01-15 01:36:43 +0000

    finish e04

--- AccesoADatos/unidad03/ejercicios/ejer03/ts-evaluacion/src/exercises/e04_tuples_enum.ts
+++ AccesoADatos/unidad03/ejercicios/ejer03/ts-evaluacion/src/exercises/e04_tuples_enum.ts
@@ -1,4 +1,5 @@
 import { Role } from "../models";
+import { isJwtPayload } from "./e05_union_guards";
 
 /**
  * E04 – Tuples y enum
@@ -8,15 +9,27 @@ export type JwtParts = [header: string, payload: string, signature: string];
 
 export function splitJwt(token: string): JwtParts {
   // "a.b.c" => [a,b,c] exactamente 3 partes, si no => Error
-  throw new Error("TODO");
+  const splited = token.split(".");
+  if (splited.length != 3) {
+    throw new Error("token has more or less then 3 parts");
+  }
+  return splited as JwtParts;
 }
 
 export function roleFromString(value: string): Role {
   // "ADMIN"|"USER" (case-insensitive) => Role; si no => Error
-  throw new Error("TODO");
+  const upperCaseValue = value.toUpperCase();
+  if (upperCaseValue == "ADMIN" || upperCaseValue == "USER") {
+    return upperCaseValue as Role;
+  } 
+  throw new Error("Invalid role")
 }
 
 export function formatUserTag(username: string, role: Role): string {
   // "juan", ADMIN => "juan#ADMIN" (username trim, no vacío)
-  throw new Error("TODO");
+  const trimmedUsername = username.trim();
+  if (trimmedUsername.length == 0) {
+    throw new Error("Invalid username");
+  }
+  return trimmedUsername + "#" + role;
 }
